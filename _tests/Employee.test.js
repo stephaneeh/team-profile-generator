a7a7249d67f0ede9ee95d91a0a83abe941f74a61
Stephanee Hayden <stephaneehayden@gmail.com>
2023-01-28 10:48:50 +1000

    Employee script and test is now working

--- _tests/Employee.test.js
+++ _tests/Employee.test.js
@@ -1,22 +1,35 @@
 // The other three classes will extend Employee.
 const Employee = require('../lib/Employee.js');
+const employee = new Employee("Roger", 1234, "roger@test.com");
 
 // test cases
-describe("Employee", () => {
-    describe("Employee object", () => {
-      it("An employee object has been created", () => {
-        const employee = new Employee("Roger", 1234, "roger@test.com");
-
-        expect(employee.name).toEqual(expect.any(String));
-        expect(employee.id).toEqual(expect.any(Number));
-        expect(employee.email).toEqual(expect.any(String));
-      });
-    });
-
-});
 // creates employee object
-// functions test
-//it ('getName() should return ????)
-//it (getID() should return ????)
-//it (getEmail() should return ????)
-//it (getRole() should return ????)
+describe("Employee object", () => {
+  it("An employee object has been created", () => {
+
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+  });
+
+  // functions test
+  //it ('getName() should return ????)
+  it("getName is run and returns a string", () => {
+    expect(employee.getName()).toEqual(expect.any(String));
+  });
+  //it (getID() should return ????)
+  it("getID is run and returns a number", () => {
+    expect(employee.getID()).toEqual(expect.any(Number));
+  });
+  //it (getEmail() should return ????)
+  it("getEmail is run and returns a string", () => {
+    expect(employee.getEmail()).toEqual(expect.any(String));
+  });
+
+  //it (getRole() should return ????)
+  it("getRole is run and returns Employee", () => {
+    expect(employee.getRole()).toEqual("Employee");
+  });
+});
+
+
