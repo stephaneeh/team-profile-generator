fb8045e456d6a84303d7101e5fde256ae5f8bbe2
Stephanee Hayden <stephaneehayden@gmail.com>
2023-01-28 11:32:09 +1000

    Intern script and test is now working

--- _tests/Intern.test.js
+++ _tests/Intern.test.js
@@ -1,7 +1,22 @@
 // The other three classes will extend Employee.
-const Employee = require('./Employee.js');
+const Intern = require('../lib/Intern.js');
+const intern = new Intern("Roger", 1234, "roger@test.com", "Harvard");
 
 // test cases
 // creates an intern object
+describe("Intern", () => {
+    it("An intern object should be created", () => {
+        
+        expect(intern.school).toEqual(expect.any(String));
+    });
+
 // it (getSchool() should turn info)
+    it("getSchool should return a string", () => {
+        expect(intern.getSchool()).toEqual(expect.any(String));
+    });
+
 // it (getRole() should return Intern)
+    it("getRole should return Intern", () => {
+        expect(intern.getRole()).toEqual("Intern");
+    })
+});
