2cec6d26ed30f9896e0f614fa89c6bdfd240e8cd
Stephanee Hayden <stephaneehayden@gmail.com>
2023-01-28 11:24:15 +1000

    Engineer script and test is now working

--- _tests/Engineer.test.js
+++ _tests/Engineer.test.js
@@ -1,8 +1,21 @@
 // The other three classes will extend Employee.
-const Employee = require('./Employee.js');
-
+const Engineer = require('../lib/Engineer.js');
+const engineer = new Engineer("Roger", 1234, "roger@test.com", "roger1234");
 // test cases
-
 // creates engineer object
+describe("Engineer object", () => {
+    it("An engineer object has been created", () => {
+  
+      expect(engineer.gitHub).toEqual(expect.any(String));
+    });
+
 // it (getGitHub() should return info)
+    it("getGithub is run and returns a string", () => {
+        expect(engineer.getGitHub()).toEqual(expect.any(String));
+    });
+
 //it (getRole() should return Engineer)
+    it("getRole is run and returns Employee", () => {
+        expect(engineer.getRole()).toEqual("Engineer");
+    });
+});
