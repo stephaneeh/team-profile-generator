ecb85d92f67c4dda78f0c2040a12b3ee0fc914d1
Stephanee Hayden <stephaneehayden@gmail.com>
2023-02-01 20:11:01 +1000

    index.js now working

--- index.js
+++ index.js
@@ -14,9 +14,16 @@ const generateHTML = require('./src/generateHTML.js');
 const teamArray = [];
 
 //create manager object
-const questions = [
-        {
-            type: 'input',
+const addManager = () => {
+    console.log(`
+- - - - - - - - - - - - - - - - 
+
+Let's add your team manager
+
+- - - - - - - - - - - - - - - - 
+    `);
+    return inquirer.prompt ([
+        {   type: 'input',
             name: 'name',
             message: 'Please enter the name of the team manager',
             validate: answer => {
@@ -25,18 +32,17 @@ const questions = [
                     return true;
                 }
         },
-        {
-            type: 'input',
+        {   type: 'input',
             name: 'id',
             message: 'Please enter the ID number for the team manager',
             validate: function (answer) {
-                if (answer == "" || answer == null) {
-                    return console.log("Please enter the ID for the manager"); }
+                if (isNaN(answer)) {
+                    return console.log("Please enter a valid ID for the manager")
+                 }
                     return true;
                 }
         },
-        {
-            type: 'input',
+        {   type: 'input', //FIXME: validate email address?
             name: 'email',
             message: 'Please enter an email address for the team manager',
             validate: function (answer) {
@@ -45,80 +51,66 @@ const questions = [
                     return true;
                 }
         },
-        {
-            type: 'input',
+        {   type: 'input',
             name: 'officeNumber',
             message: 'Please enter an office number for the team manager',
             validate: function (answer) {
-                if (answer == "" || answer == null) {
+                if (isNaN(answer)) {
                     return console.log("Please enter an office number for the manager"); }
                     return true;
                 }
         },
-        {
+    ])
+    .then(managerResults => {
+        const { name, id, email, officeNumber } = managerResults;
+        const manager = new Manager (name, id, email, officeNumber);
+
+        teamArray.push(manager);
+        console.log(manager); //FIXME: REMOVE AT END
+        addEmployee();
+        })
+    };
+
+
+//create manager object
+const addEmployee = () => {
+        console.log(`
+- - - - - - - - - - - - - - - - 
+
+Now let's add your team members
+
+- - - - - - - - - - - - - - - - 
+        `);
+        return inquirer.prompt ([
+        {   
             type: 'list',
             name: 'role',
             message: 'Which role would you like to add?',
             choices: ['Engineer', 'Intern'],
         },
-        {
-            type: 'input',
-            name: 'employeeName',
-            message: 'Please enter the name of your employee',
-            validate: function (answer) {
-                if (answer == "" || answer == null) {
-                    return console.log("Please enter a contact email address"); }
-                    return true;
-                }
-        },
-        {
-            type: 'input',
-            name: 'employeeEmail',
-            message: 'Please enter an email address for your employee',
-            validate: function (answer) {
-                if (answer == "" || answer == null) {
-                    return console.log("Please enter an email address for your employee"); }
-                    return true;
-                }
-        },
-        {
-            type: 'input',
-            name: 'engineerGithub',
-            message: 'Please enter a github for your employee',
-            when: (answers) => answers.role === 'Engineer',
-        },
-    ];
-    // .then(managerInput => {
-    //     const {name, id, email, officeNumber} = managerInput;
-    //     const manager = new Manager (name, id, email, officeNumber);
-
-    //     teamArray.push(manager);
-    //     console.log(manager);
-
-    //     console.log(teamArray);
-// });
-
-//create employee objects
-const createEmployee = () => {
-    return inquirer.prompt ([
-        {
-            type: 'list',
-            name: 'role',
-            message: 'Which role would you like to add?',
-            choices: ['Engineer', 'Intern'],
-        },
-        {
+        {   
             type: 'input',
             name: 'name',
             message: 'Please enter the name of your employee',
-            validate: function (answer) {
+            validate: answer => {
                 if (answer == "" || answer == null) {
-                    return console.log("Please enter a contact email address"); }
+                    return console.log("Please enter the name of your employee"); }
                     return true;
                 }
         },
-        {
+        {   
             type: 'input',
+            name: 'id',
+            message: 'Please enter the ID number for the team member',
+            validate: function (answer) {
+                if (isNaN(answer)) {
+                    return console.log("Please enter a valid ID for the team member")
+                 }
+                    return true;
+                }
+        },
+        {   
+            type: 'input', //FIXME: validate email address?
             name: 'email',
             message: 'Please enter an email address for your employee',
             validate: function (answer) {
@@ -127,31 +119,77 @@ const createEmployee = () => {
                     return true;
                 }
         },
+        {   //required for Engineer only
+            type: 'input',
+            name: 'github',
+            message: 'Please enter a github for your employee',
+            when: (input) => input.role === 'Engineer',
+            validate: function (answer) {
+                if (answer == "" || answer == null) {
+                    return console.log("Please enter the github for your employee"); }
+                    return true;
+                }
+        },
+        {   //required for Intern only
+            type: 'input',
+            name: 'school',
+            message: 'Please enter the school your intern attended',
+            when: (input) => input.role === 'Intern',
+            validate: function (answer) {
+                if (answer == "" || answer == null) {
+                    return console.log("Please enter a school name"); }
+                    return true;
+                }
+        },    
     ])
-    .then(employeeInput => {
-        let {name, role, email} = employeeInput;
-        const employee = new Employee (name, role, email);
+    .then(employeeResults => {
+        let { name, id, email, role, github, school } = employeeResults; 
+        let employee; 
 
-        teamArray.push(employee);
-        console.log(employee);
+        if (role === 'Engineer') {
+            employee = new Engineer (name, id, email, github);
+        } else if (role === 'Intern') {
+            employee = new Engineer (name, id, email, school);
+        }
+            teamArray.push(employee); //add results to teamArray()
+            console.log(employee);
+            confirmAddEmployee();
+        })
+    };
 
-        console.log(teamArray); //FIXME:
+//create manager object
+const confirmAddEmployee = () => {
+    inquirer.prompt ([
+        {   //add more employees
+            type: 'confirm',
+            name: 'addMoreEmployees',
+            message: 'Would you like to add more employees?',
+            default: false,
+        },
+    ])
+    .then((data) => {
+        console.log(data);
+        if (data.addMoreEmployees === true){
+            addEmployee();
+        } else {
+            console.log(`
+- - - - - - - - - - - - - - - - 
+
+Congratulations - your team has 
+now been created!
+
+- - - - - - - - - - - - - - - - 
+            `);
+            renderFile(data);
+        };
     })
 };
 
-function writeFile() {
-    inquirer.prompt(questions)
-        .then((data) => {
+
+function renderFile(data) {
         fs.writeFile('index.html', generateHTML(data), (err) =>
-        err ? console.error(err) : console.log('ReadMe file created!'))
-    });
-};
-
-
-// Function to initialize app
-function init() {
-    writeFile()
-};
+        err ? console.error(err) : console.log('Your teams page has been created!'))
+    };
 
 // Function call to initialize app
-init();
+addManager();
